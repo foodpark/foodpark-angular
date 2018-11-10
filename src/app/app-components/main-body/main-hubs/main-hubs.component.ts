3cd553f53d840ec467d03fd360f0bdef48e6eb6b
lakshitha <sai@infomerica.com>
2018-11-10 22:09:05 +0530

    removed spaces for child route

--- src/app/app-components/main-body/main-hubs/main-hubs.component.ts
+++ src/app/app-components/main-body/main-hubs/main-hubs.component.ts
@@ -1,35 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-main-hubs',
-  templateUrl: './main-hubs.component.html',
-  styleUrls: ['./main-hubs.component.scss']
+    selector: 'app-main-hubs',
+    templateUrl: './main-hubs.component.html',
+    styleUrls: ['./main-hubs.component.scss']
 })
 export class MainHubsComponent implements OnInit {
 
-  createMainhubrForm: FormGroup;
-  submitted = false;
+    createMainhubrForm: FormGroup;
+    submitted = false;
 
-  constructor(private formBuilder: FormBuilder,
-              private http: HttpClient,
-              private router: Router) {
-  }
+    constructor(private formBuilder: FormBuilder,
+                private http: HttpClient,
+                private router: Router) {
+    }
 
-  ngOnInit() {
-    this.createMainhubrForm = this.formBuilder.group({
-        mainhubname:['', Validators.required],
-        latitude: ['', Validators.required],
-        longitude: ['', Validators.required],
-        country: ['', Validators.required],
-        territory: ['', Validators.required],
-        mainhub: ['', Validators.required]
-    });
-  }
+    ngOnInit() {
+        this.createMainhubrForm = this.formBuilder.group({
+            mainhubname: ['', Validators.required],
+            latitude: ['', Validators.required],
+            longitude: ['', Validators.required],
+            country: ['', Validators.required],
+            territory: ['', Validators.required],
+            mainhub: ['', Validators.required]
+        });
+    }
 
-  get f() {
-      return this.createMainhubrForm.controls;
-  }
+    get f() {
+        return this.createMainhubrForm.controls;
+    }
+
+    onCreateMainHubClick() {
+    }
 }
