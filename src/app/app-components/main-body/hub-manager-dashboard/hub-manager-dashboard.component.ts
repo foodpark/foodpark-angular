4372311eb29920bb97e726b51fc86e953c47dbd9
satyam <foodpark@gmail.com>
2018-11-10 16:52:30 +0530

    adding new hub manager screen

--- src/app/app-components/main-body/hub-manager-dashboard/hub-manager-dashboard.component.ts
+++ src/app/app-components/main-body/hub-manager-dashboard/hub-manager-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-hub-manager-dashboard',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hub-manager-dashboard.component.scss']
 })
 export class HubManagerDashboardComponent implements OnInit {
+  hubmanagerForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private http: HttpClient,
+              private router: Router) {
+  }
 
   ngOnInit() {
+    this.hubmanagerForm = this.formBuilder.group({
+        firstname:['', Validators.required],
+        lastname: ['', Validators.required],
+        username: ['', Validators.required],
+        password: ['', Validators.required],
+        repeatpassword: ['', Validators.required],
+        country: ['', Validators.required],
+        territory: ['', Validators.required],
+        mainhub: ['', Validators.required]
+    });
+  }
+
+  get f() {
+      return this.hubmanagerForm.controls;
   }
 
 }
