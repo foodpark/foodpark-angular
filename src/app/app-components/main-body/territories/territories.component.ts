9ea15c69d8522970fd9909ec11d8589f9c5d1ad8
lakshitha <sai@infomerica.com>
2018-11-11 17:48:04 +0530

    territories component and css changes

--- src/app/app-components/main-body/territories/territories.component.ts
+++ src/app/app-components/main-body/territories/territories.component.ts
@@ -7,15 +7,27 @@ import {HttpClient} from "@angular/common/http";
     styleUrls: ['./territories.component.scss']
 })
 export class TerritoriesComponent implements OnInit {
+    gridMetadata;
+    territories = [];
+    country = [];
 
     constructor(private http: HttpClient) {
         this.http.get('https://api.instamarkt.co/api/v1/rel/territories').subscribe(res => {
-            console.log(res);
+            Object.values(res).forEach(item => {
+                this.territories.push(item['territory']);
+                this.country.push(item['country']);
+            });
+            this.gridMetadata = res;
         });
     }
 
     ngOnInit() {
     }
 
+    onEditClick() {
+    }
+
+    onDeleteClick() {
+    }
 
 }
