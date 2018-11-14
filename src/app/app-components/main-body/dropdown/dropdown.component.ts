bd1ea8e1a7da6aa62768296ff12b487494feff67
satyam <foodpark@gmail.com>
2018-11-15 00:06:19 +0530

    fix for empty string in  dropdown  name

--- src/app/app-components/main-body/dropdown/dropdown.component.ts
+++ src/app/app-components/main-body/dropdown/dropdown.component.ts
@@ -1,27 +1,27 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ErrorStateMatcher} from '@angular/material/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'app-dropdown',
     templateUrl: './dropdown.component.html',
     styleUrls: ['./dropdown.component.scss']
 })
+
 export class DropdownComponent implements OnInit {
-    @Input() dropdownName: string;
+    @Input() dropdownName = '';
     dropdownData;
 
     constructor(private http: HttpClient) {
         if (this.dropdownName.toLowerCase() === 'countries ') {
-            this.http.get('https://api.instamarkt.co/api/v1/rel/countries').subscribe(res => {
+            this.http.get(environment.apiUrl + '/api/v1/rel/countries').subscribe(res => {
                 this.dropdownData = res;
             });
         } else if (this.dropdownName.toLowerCase() === 'territories ') {
-            this.http.get('https://api.instamarkt.co/api/v1/rel/territories').subscribe(res => {
+            this.http.get(environment.apiUrl + '/api/v1/rel/territories').subscribe(res => {
                 this.dropdownData = res;
             });
         }
-
     }
 
     ngOnInit() {
