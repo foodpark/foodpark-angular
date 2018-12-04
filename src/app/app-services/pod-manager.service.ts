d2f6495a515733dc0a173bca236b9f3bf2534554
narendra_k <narendra@hashtaag.com>
2018-12-04 18:27:20 +0530

    completed load resources

--- src/app/app-services/pod-manager.service.ts
+++ src/app/app-services/pod-manager.service.ts
@@ -11,11 +11,21 @@ export class PodsManagerService {
 
     constructor(private http: HttpClient) {}
 
-
+    apigetcategories(){
+      return this.http.get(environment.apiUrl + '/api/v1/rel/categories')
+    }
     apigetLoadRequests() {
-        return this.http.get(environment.apiUrl + '/api/v1/rel/loads');
+      return this.http.get(environment.apiUrl + '/api/v1/rel/loads');
     }
     apicreateLoadRequests(data) {
-        return this.http.post(environment.apiUrl + '/api/v1/rel/loads',data);
+      return this.http.post(environment.apiUrl + '/api/v1/rel/loads',data);
+    }
+
+    apigetLoadItems(id) {
+      return this.http.get(environment.apiUrl + '/api/v1/rel/load_items?load_id='+ id);
+    }
+
+    apicreateLoadItems(data) {
+      return this.http.post(environment.apiUrl + '/api/v1/rel/load_items', data);
     }
 }
