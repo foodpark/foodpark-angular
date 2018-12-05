a467a4acbf01024fb1239ccada8f2ad88aac03c9
satyam <foodpark@gmail.com>
2018-12-05 16:21:44 +0530

    bug fixes

--- src/app/app-components/main-body/main-hub-manager/edit-pod/edit-pods.component.ts
+++ src/app/app-components/main-body/main-hub-manager/edit-pod/edit-pods.component.ts
@@ -1,92 +1,144 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import {Router, ParamMap, ActivatedRoute} from '@angular/router';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {PodsService} from 'src/app/app-services/pods.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ParamMap, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PodsService } from 'src/app/app-services/pods.service';
+import { FileUploadService } from 'src/app/app-services/fileupload.service';
+import { Subscription } from 'rxjs';
+import { PodModel } from 'src/app/model';
 
 @Component({
     selector: 'app-edit-pods',
-    templateUrl: './edit-pods.component.html',
+    templateUrl: './edit-pods.component.html'
 })
 
 export class EditPodsComponent implements OnInit, OnDestroy {
     editpodform: FormGroup;
-    podId;
-    pods;
-    churchType = ['Church', 'Non-Profit', 'Non-Religious', 'Non-Denominational', 'Other'];
-    connectedBy = ['Personal Referral', 'Google Search', 'Social Media', 'Other'];
+    podId: string;
+    pod: PodModel;
+    churchType = [
+        'Church',
+        'Non-Profit',
+        'Non-Religious',
+        'Non-Denominational',
+        'Other'
+    ];
 
-    constructor(private formBuilder: FormBuilder,
-                private route: ActivatedRoute,
-                private router: Router,
-                private podService: PodsService) {
-    }
+    connectedBy = [
+        'Personal Referral',
+        'Google Search',
+        'Social Media',
+        'Other'
+    ];
+    private wordfileURL: string;
+    private wordFileToUpload: File;
+    private fileUploadSubscription: Subscription;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private podService: PodsService,
+        private fileUploadService: FileUploadService
+    ) {}
 
     ngOnInit() {
         this.editpodform = this.formBuilder.group({
-            name: ['', Validators.required],
+            church_name: ['', Validators.required],
             latitude: ['', Validators.required],
             longitude: ['', Validators.required],
             sponsor: ['', Validators.required],
             title: ['', Validators.required],
             connected_with: ['', Validators.required],
             type: ['', Validators.required],
-            // wordFile: [null, Validators.required]
+            wordFile: [null, Validators.required]
         });
+
         this.route.paramMap.subscribe((paramMap: ParamMap) => {
             if (paramMap.has('podId')) {
                 this.podId = paramMap.get('podId');
-                this.podService.getPodFromPodId(this.podId).subscribe(res => {
-                    this.pods = res;
-                    this.editpodform.get('name').setValue(this.pods['name'], {emitEvent: false});
-                    this.editpodform.get('latitude').setValue(this.pods['latitude'], {emitEvent: false});
-                    this.editpodform.get('longitude').setValue(this.pods['longitude'], {emitEvent: false});
-                    this.editpodform.get('sponsor').setValue(this.pods['sponsor'], {emitEvent: false});
-                    this.editpodform.get('title').setValue(this.pods['title'], {emitEvent: false});
-                    this.editpodform.get('connected_with').setValue(this.pods['connected_with'], {emitEvent: false});
-                    this.editpodform.get('type').setValue(this.pods['type'], {emitEvent: false});
-                    document.getElementById('church_type').innerText = this.editpodform.get('type').value;
-                    document.getElementById('connected_with').innerText = this.editpodform.get('connected_with').value;
+                this.podService.getPodFromPodId(parseInt(this.podId, 10)).subscribe(res => {
+                    this.pod = res;
+                    this.editpodform
+                        .get('name')
+                        .setValue(this.pod['name'], { emitEvent: false });
+                    this.editpodform
+                        .get('latitude')
+                        .setValue(this.pod['latitude'], { emitEvent: false });
+                    this.editpodform
+                        .get('longitude')
+                        .setValue(this.pod['longitude'], { emitEvent: false });
+                    this.editpodform
+                        .get('sponsor')
+                        .setValue(this.pod['sponsor'], { emitEvent: false });
+                    this.editpodform
+                        .get('title')
+                        .setValue(this.pod['title'], { emitEvent: false });
+                    this.editpodform
+                        .get('connected_with')
+                        .setValue(this.pod['connected_with'], {
+                            emitEvent: false
+                        });
+                    this.editpodform
+                        .get('type')
+                        .setValue(this.pod['type'], { emitEvent: false });
+                    document.getElementById(
+                        'church_type'
+                    ).innerText = this.editpodform.get('type').value;
+                    document.getElementById(
+                        'connected_with'
+                    ).innerText = this.editpodform.get('connected_with').value;
                 });
             }
+        });
+
+        this.fileUploadSubscription = this.fileUploadService.getFileUploadListener()
+        .subscribe((fileURL) => {
+            this.wordfileURL = fileURL;
+            this.uploadPod();
         });
     }
 
     onChurchTypeClick(type: string) {
-        const button = document.getElementById('church_type');
-        button.innerText = type;
+        document.getElementById('church_type').innerText = type;
         this.editpodform.get('type').setValue(type);
     }
 
     onConnectedByClick(type: string) {
-        const button = document.getElementById('connected_with');
-        button.innerText = type;
+        document.getElementById('connected_with').innerText = type;
         this.editpodform.get('connected_with').setValue(type);
     }
 
+    onFilePicked(files: FileList) {
+        this.wordFileToUpload = files.item(0);
+        this.editpodform.get('wordFile').setValue(this.wordFileToUpload);
+        document.getElementById('wordfile_name').innerText = this.wordFileToUpload.name;
+    }
+
     savePod() {
-        const updatePodObj = {
-            'name': this.editpodform.get('name').value,
-            'title': this.editpodform.get('title').value,
-            'connected_with': this.editpodform.get('connected_with').value,
-            'sponsor': this.editpodform.get('sponsor').value,
-            'latitude': this.editpodform.get('latitude').value,
-            'longitude': this.editpodform.get('longitude').value,
-            'type': this.editpodform.get('type').value,
-            'approved': true
+        if (this.wordFileToUpload == null) {
+            this.uploadPod();
+        } else {
+            this.fileUploadService.uploadFile(this.wordFileToUpload);
+        }
+    }
+
+    uploadPod() {
+        const updatePodData = {
+            'name': this.editpodform.value.church_name,
+            'title': this.editpodform.value.title,
+            'connected_with': this.editpodform.value.connectedBy,
+            'sponsor': this.editpodform.value.sponsor,
+            'latitude': this.editpodform.value.latitude,
+            'longitude': this.editpodform.value.longitude,
+            'type': this.editpodform.value.type,
+            'wordfile': this.wordfileURL
         };
 
-        this.podService.updatePod(this.podId, updatePodObj)
-            .subscribe(() => {
-                this.router.navigate(['/hubmanager/podapplications']);
-            });
+        this.podService.updatePod(parseInt(this.podId, 10), updatePodData)
+        .subscribe(() => {
+            this.router.navigate(['/hubmanager/podapplications']);
+        });
     }
 
-    onFilePicked(event: Event) {
-        const file = (event.target as HTMLInputElement).files[0];
-        this.editpodform.get('wordFile').setValue(file);
-        this.editpodform.get('wordFile').updateValueAndValidity();
-    }
-
-    ngOnDestroy() {
-    }
+    ngOnDestroy() {}
 }
