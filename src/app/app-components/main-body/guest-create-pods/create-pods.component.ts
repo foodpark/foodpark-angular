e3f9f9402f63494bb1479b57eedf3737e1c3b41d
narendra_k <narendra@hashtaag.com>
2019-01-04 23:11:41 +0530

    code cleanup

--- src/app/app-components/main-body/guest-create-pods/create-pods.component.ts
+++ src/app/app-components/main-body/guest-create-pods/create-pods.component.ts
@@ -35,6 +35,27 @@ export class GuestCreatePodsComponent implements OnInit, OnDestroy {
     connectedWith = ['Personal Referral', 'Google Search', 'Social Media', 'Other'];
 
     constructor(private formBuilder: FormBuilder, private route: Router, private countryService: CountryService, private territoryService: TerritoryService, private mainhubService: MainhubService, private podService: PodsService, private dataService: DataService, private fileUploadService: FileUploadService) {
+      this.countryService.getCountries();
+      this.fileUploadSubscription = this.fileUploadService.getFileUploadListener()
+        .subscribe((fileURL) => {
+          console.log('File Uploaded: ' + fileURL);
+          this.wordfileURL = fileURL;
+          this.createPodManager();
+      });
+
+      this.countriesSubscription = this.countryService.getCountriesUpdateListener()
+        .subscribe((countries: CountryModel[]) => {
+          this.countries = countries;
+      });
+      this.territoriesSubscription = this.territoryService.getTerritoriesUpdateListener()
+        .subscribe((territories: TerritoryModel[]) => {
+          this.territories = territories;
+      });
+
+      this.mainhubsSubscription = this.mainhubService.getMainhubsUpdateListener()
+        .subscribe((res: MainhubModel[]) => {
+          this.mainhubs = res;
+      });
 
     }
 
@@ -58,49 +79,25 @@ export class GuestCreatePodsComponent implements OnInit, OnDestroy {
             longitude: ['', Validators.required],
             wordFile: [null, Validators.required]
         });
-
-        this.fileUploadSubscription = this.fileUploadService.getFileUploadListener()
-            .subscribe((fileURL) => {
-                console.log('File Uploaded: ' + fileURL);
-                this.wordfileURL = fileURL;
-                this.createPodManager();
-            });
-            this.countryService.getCountries();
-
-        this.countriesSubscription = this.countryService.getCountriesUpdateListener()
-            .subscribe((countries: CountryModel[]) => {
-                this.countries = countries;
-            });
-        this.territoriesSubscription = this.territoryService.getTerritoriesUpdateListener()
-        .subscribe((territories: TerritoryModel[]) => {
-            this.territories = territories;
-        });
-
-        this.mainhubsSubscription = this.mainhubService.getMainhubsUpdateListener()
-        .subscribe((res: MainhubModel[]) => {
-            this.mainhubs = res;
-        });
-
     }
 
     onCountryClick(index: number, id: number) {
-        const button = document.getElementById('country_button');
-        button.innerText = this.countries[index]['name'];
-        this.registerpodform.get('country').setValue(this.countries[index]['name']);
-        this.registerpodform.get('country_id').setValue(this.countries[index]['id']);
-        this.getTerritory(id);
+      const button = document.getElementById('country_button');
+      button.innerText = this.countries[index]['name'];
+      this.registerpodform.get('country').setValue(this.countries[index]['name']);
+      this.registerpodform.get('country_id').setValue(this.countries[index]['id']);
+      this.getTerritory(id);
     }
 
     getTerritory(id: number) {
-        this.territoryService.getTerritoriesInCountry(id);
-
+      this.territoryService.getTerritoriesInCountry(id);
     }
 
     onTerritoryClick(index: number, item) {
-        const button = document.getElementById('territory_button');
-        button.innerText = this.territories[index]['territory'];
-        this.registerpodform.get('territory_id').setValue(this.territories[index]['id']);
-        this.getMainhubs(item.id, item.country);
+      const button = document.getElementById('territory_button');
+      button.innerText = this.territories[index]['territory'];
+      this.registerpodform.get('territory_id').setValue(this.territories[index]['id']);
+      this.getMainhubs(item.id, item.country);
     }
 
     getMainhubs(id: number, country: string ){
@@ -108,54 +105,53 @@ export class GuestCreatePodsComponent implements OnInit, OnDestroy {
     }
 
     onMainHubClick(index: number, item) {
-        const button = document.getElementById('mainhub_button');
-        button.innerText = this.mainhubs[index]['name'];
-        this.registerpodform.get('main_hub_id').setValue(this.mainhubs[index]['id']);
-        //this.getMainhubs(item.id, item.country);
+      const button = document.getElementById('mainhub_button');
+      button.innerText = this.mainhubs[index]['name'];
+      this.registerpodform.get('main_hub_id').setValue(this.mainhubs[index]['id']);
     }
 
     onChurchTypeClick(type: string) {
-        const button = document.getElementById('church_type');
-        button.innerText = type;
-        this.registerpodform.get('type').setValue(type);
+      const button = document.getElementById('church_type');
+      button.innerText = type;
+      this.registerpodform.get('type').setValue(type);
     }
 
     onConnectedByClick(type: string) {
-        const button = document.getElementById('connected_with');
-        button.innerText = type;
-        this.registerpodform.get('connected_with').setValue(type);
+      const button = document.getElementById('connected_with');
+      button.innerText = type;
+      this.registerpodform.get('connected_with').setValue(type);
     }
 
     onFilePicked(files: FileList) {
-        this.wordFileToUpload = files.item(0);
-        this.hideFileContainer = this.dataService.nullCheck(this.wordFileToUpload);
-        this.registerpodform.get('wordFile').setValue(this.wordFileToUpload);
-        document.getElementById('wordfile_name').innerText = this.wordFileToUpload.name;
+      this.wordFileToUpload = files.item(0);
+      this.hideFileContainer = this.dataService.nullCheck(this.wordFileToUpload);
+      this.registerpodform.get('wordFile').setValue(this.wordFileToUpload);
+      document.getElementById('wordfile_name').innerText = this.wordFileToUpload.name;
     }
 
     createPod() {
-        this.fileUploadService.uploadFile(this.wordFileToUpload);
+      this.fileUploadService.uploadFile(this.wordFileToUpload);
     }
 
     private createPodManager() {
-        const obj = {
-            'role': 'PODMGR',
-            'email': this.registerpodform.get('email').value,
-            'first_name': this.registerpodform.get('firstname').value,
-            'last_name': this.registerpodform.get('lastname').value,
-            'password': this.registerpodform.get('password').value,
-            'country_id': this.registerpodform.get('country_id').value,
-            'territory_id': this.registerpodform.get('territory_id').value,
-            'church_name': this.registerpodform.get('church_name').value,
-            'connected_with': this.registerpodform.get('connected_with').value
-        };
+      const obj = {
+        'role': 'PODMGR',
+        'email': this.registerpodform.get('email').value,
+        'first_name': this.registerpodform.get('firstname').value,
+        'last_name': this.registerpodform.get('lastname').value,
+        'password': this.registerpodform.get('password').value,
+        'country_id': this.registerpodform.get('country_id').value,
+        'territory_id': this.registerpodform.get('territory_id').value,
+        'church_name': this.registerpodform.get('church_name').value,
+        'connected_with': this.registerpodform.get('connected_with').value
+      };
 
-        this.podService.registerPodManager(obj)
-            .subscribe((response) => {
-                console.log('Created pod manager');
-                this.church_id = response['user']['church_id'];
-                this.sendPodData();
-            });
+      this.podService.registerPodManager(obj)
+        .subscribe((response) => {
+          console.log('Created pod manager');
+          this.church_id = response['user']['church_id'];
+          this.sendPodData();
+      });
     }
 
     private sendPodData() {
