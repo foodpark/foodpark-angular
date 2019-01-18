ff343e65063dd9cb77517b5521518e469be01625
satyam <foodpark@gmail.com>
2019-01-18 23:13:54 +0530

    showing error when response is bad

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -2,21 +2,34 @@ import {
     HttpInterceptor,
     HttpRequest,
     HttpHandler,
-    HttpErrorResponse
+    HttpEvent,
+    HttpErrorResponse,
+    HttpResponse
 } from '@angular/common/http';
 import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 import {MatDialog} from '@angular/material';
 import {ErrorComponent} from './error/error.component';
 import {Injectable} from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private dialog: MatDialog) {
+    constructor(private dialog: MatDialog,
+        public toasterService: ToastrService) {
     }
 
-    intercept(req: HttpRequest<any>, next: HttpHandler) {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(
+            // tap(evt => {
+            //     if (evt instanceof HttpResponse) {
+            //         if (evt.body && evt.body.success) {
+            //             this.toasterService.success(evt.body.success.message, evt.body.success.title, { positionClass: 'toast-bottom-center' });
+            //         }
+            //     }
+            // }),
             catchError((error: HttpErrorResponse) => {
                 console.log(error);
                 let errorMessage = 'An unknown error occurred!';
@@ -25,6 +38,12 @@ export class ErrorInterceptor implements HttpInterceptor {
                 }
                 // Need to fix this
                 // this.dialog.open(ErrorComponent, {data: {message: errorMessage}});
+                try {
+                    this.toasterService.error('Error', errorMessage, { positionClass: 'toast-bottom-center' });
+                } catch (e) {
+                    this.toasterService.error('An error occured', '', { positionClass: 'toast-bottom-center' });
+                }
+
                 return throwError(error);
             })
         );
